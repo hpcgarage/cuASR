9d43e9f85784bfb800c946b1d7cc5731b9627a10
thakkarv <thakkarv@gatech.edu>
2020-02-19 16:07:28 -0500

    Refactor CPU benchmarks so as to not allocate at every iteration

--- bench/cpu_bench.cpp
+++ bench/cpu_bench.cpp
@@ -2,25 +2,21 @@
 #include "include/cpu_gemm.hpp"
 
 static void BM_CPUSimpleGemm(benchmark::State &state) {
-  while (state.KeepRunning()) {
-    state.PauseTiming();
-    const auto N = state.range(0);
-    auto x       = fwgpu::Matrix<float>(N, N, 1.5f);
-    auto y       = fwgpu::Matrix<float>(N, N, 1.5f);
-    state.ResumeTiming();
-    auto z = fwgpu::cpu_gemm_naive_entry(x, y);
+  const auto N = state.range(0);
+  auto x       = fwgpu::Matrix<float>(N, N, 1.5f);
+  auto y       = fwgpu::Matrix<float>(N, N, 1.5f);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(fwgpu::cpu_gemm_naive_entry(x, y));
   }
 }
 BENCHMARK(BM_CPUSimpleGemm)->RangeMultiplier(2)->Range(64, 1024);
 
 static void BM_CPUNaiveGemm(benchmark::State &state) {
-  while (state.KeepRunning()) {
-    state.PauseTiming();
-    const auto N = state.range(0);
-    auto x       = fwgpu::Matrix<float>(N, N, 1.5f);
-    auto y       = fwgpu::Matrix<float>(N, N, 1.5f);
-    state.ResumeTiming();
-    auto z = fwgpu::naive_mm(x, y);
+  const auto N = state.range(0);
+  auto x       = fwgpu::Matrix<float>(N, N, 1.5f);
+  auto y       = fwgpu::Matrix<float>(N, N, 1.5f);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(fwgpu::naive_mm(x, y));
   }
 }
 BENCHMARK(BM_CPUNaiveGemm)->RangeMultiplier(2)->Range(64, 1024);
