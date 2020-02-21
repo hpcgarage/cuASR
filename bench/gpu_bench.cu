dd83502ae96ed92ad39d16f01a7dea31a0b7d054
thakkarv <thakkarv@gatech.edu>
2020-02-21 11:37:03 -0500

    change to cuda event timers for benchmark timing

--- bench/gpu_bench.cu
+++ bench/gpu_bench.cu
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <iostream>
 #include <tuple>
 
 #include "benchmark/benchmark.h"
@@ -7,7 +8,6 @@
 #include "include/internal/utils.cuh"
 
 static void BM_GpuGemmNaive(benchmark::State &state) {
-  using namespace std::chrono;
   const auto N = state.range(0);
 
   // init input matrices for this benchmark size N
@@ -20,50 +20,65 @@ static void BM_GpuGemmNaive(benchmark::State &state) {
   float *d_B = std::get<1>(dptrs);
   float *d_C = std::get<2>(dptrs);
 
+  float milliseconds = 0.0;
+  cudaEvent_t start;
+  cudaEvent_t stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
   // loop over benchmark for this size
   dim3 threads(16, 16);
   dim3 blocks((N - 1) / 16 + 1, (N - 1) / 16 + 1);
   for (auto _ : state) {
-    auto start = high_resolution_clock::now();
-
+    cudaEventRecord(start);
     fwgpu::gpu_gemm_naive<float><<<blocks, threads>>>(N, N, N, d_A, d_B, d_C);
-    cudaDeviceSynchronize();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
 
-    auto end = high_resolution_clock::now();
-    auto d   = duration_cast<duration<double>>(end - start);
-    state.SetIterationTime(d.count());
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    state.SetIterationTime(milliseconds / 1000);
   }
 
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
   // free device buffers
   fwgpu::internal::dealloc_gemm_mats_on_gpu<float>(dptrs);
 }
 BENCHMARK(BM_GpuGemmNaive)->RangeMultiplier(2)->Range(64, 1024)->UseManualTime();
 
 static void BM_CublasSgemm(benchmark::State &state) {
-  using namespace std::chrono;
   const auto N = state.range(0);
 
-  // allocate device buffers for this benchmark size N
-  auto A     = fwgpu::Matrix<float>(N, N, 1.5f);
-  auto B     = fwgpu::Matrix<float>(N, N, 1.5f);
-  auto dptrs = fwgpu::internal::alloc_gemm_mats_on_gpu(A, B);
+  // init input matrices for this benchmark size N
+  auto A = fwgpu::Matrix<float>(N, N, 1.5f);
+  auto B = fwgpu::Matrix<float>(N, N, 1.5f);
+
+  // allocate device buffers
+  auto dptrs = fwgpu::internal::alloc_gemm_mats_on_gpu<float>(A, B);
   float *d_A = std::get<0>(dptrs);
   float *d_B = std::get<1>(dptrs);
   float *d_C = std::get<2>(dptrs);
 
+  float milliseconds = 0.0;
+  cudaEvent_t start;
+  cudaEvent_t stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+
   // loop over benchmark for this size
   for (auto _ : state) {
-    auto start = high_resolution_clock::now();
-
+    cudaEventRecord(start);
     fwgpu::cublas_sgemm(d_A, d_B, d_C, N, N, N);
-    cudaDeviceSynchronize();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
 
-    auto end = high_resolution_clock::now();
-    auto d   = duration_cast<duration<double>>(end - start);
-    state.SetIterationTime(d.count());
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    state.SetIterationTime(milliseconds / 1000);
   }
 
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
   // free device buffers
-  fwgpu::internal::dealloc_gemm_mats_on_gpu(dptrs);
+  fwgpu::internal::dealloc_gemm_mats_on_gpu<float>(dptrs);
 }
 BENCHMARK(BM_CublasSgemm)->RangeMultiplier(2)->Range(64, 4096)->UseManualTime();
