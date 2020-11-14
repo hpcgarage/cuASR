bf225495cab49a3c58592f0c16a838ec24e08b3e
thakkarv <thakkarv@gatech.edu>
2020-11-13 20:51:43 -0500

    remove run_clang_format.sh

--- run_clang_format.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-find ./src     -regex '.*\.\(cpp\|hpp\|cu\|cuh\)' -exec clang-format -style=file -i {} \;
-find ./include -regex '.*\.\(cpp\|hpp\|cu\|cuh\)' -exec clang-format -style=file -i {} \;
-find ./bench   -maxdepth 1 -regex '.*\.\(cpp\|hpp\|cu\|cuh\)' -exec clang-format -style=file -i {} \;
-find ./test    -maxdepth 1 -regex '.*\.\(cpp\|hpp\|cu\|cuh\)' -exec clang-format -style=file -i {} \;
